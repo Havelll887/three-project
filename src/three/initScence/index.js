77f43fe9c06bdc467ab2790ad54518af97eff558
havelll887 <aoyue1122@outlook.com>
2023-07-13 22:29:55 +0800

    init scence render

--- src/three/initScence/index.js
+++ src/three/initScence/index.js
@@ -15,8 +15,18 @@ export default class threeInit {
             1000
         );
         this.camera.position.set(0, 0, 5.5);
+        this.camera.lookAt(new Vector3(0, 0, 0)); // 设置相机看先中心点
+        this.camera.up = new Vector3(0, 1, 0); // 设置相机自身方向
+        this.camera.updateProjectionMatrix();
         // 控制器
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        window.addEventListener("resize",this.winResize);
+    }
+
+    winResize(){
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
     // 连续渲染
@@ -24,6 +34,15 @@ export default class threeInit {
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(() => {
             this.animate();
+        });
+    }
+
+    /**
+    * 向场景中添加模型
+    */
+    addObject(...object) {
+        object.forEach((elem) => {
+            this.scene.add(elem); // 场景添加模型
         });
     }
 }
